9ad42de5d945d61115b706f1b2ef2b8967008a71
Cha Kyung Jun <dennnischa50@gmail.com>
2017-06-05 23:30:43 +0900

    final commit

--- src/kr/ac/kookmin/cs/bigdata/SentiWordNet.java
+++ src/kr/ac/kookmin/cs/bigdata/SentiWordNet.java
@@ -8,133 +8,124 @@ import java.util.Map;
 import java.lang.String;
 import edu.stanford.nlp.simple.*;
 
-
 public class SentiWordNet {
 
-private Map<String, Double> dictionary;
+	private Map<String, Double> dictionary;
+	//reference from http://sentiwordnet.isti.cnr.it/ demo code
+	public SentiWordNet(String pathToSWN) throws IOException {
+		// This is our main dictionary representation
+		if (pathToSWN.length() < 1) {
+			System.err.println("Usage: java SentiWordNetDemoCode <pathToSentiWordNetFile>");
+			return;
+		}
+		dictionary = new HashMap<String, Double>();
 
-public SentiWordNet(String pathToSWN) throws IOException {
-	// This is our main dictionary representation
-	if(pathToSWN.length()<1) {
-		System.err.println("Usage: java SentiWordNetDemoCode <pathToSentiWordNetFile>");
-		return;
-	}
-	dictionary = new HashMap<String, Double>();	
+		// From String to list of doubles.
+		HashMap<String, HashMap<Integer, Double>> tempDictionary = new HashMap<String, HashMap<Integer, Double>>();
 
-	// From String to list of doubles.
-	HashMap<String, HashMap<Integer, Double>> tempDictionary = new HashMap<String, HashMap<Integer, Double>>();
+		BufferedReader csv = null;
+		try {
+			csv = new BufferedReader(new FileReader(pathToSWN));
+			int lineNumber = 0;
 
-	BufferedReader csv = null;
-	try {
-		csv = new BufferedReader(new FileReader(pathToSWN));
-		int lineNumber = 0;
+			String line;
+			while ((line = csv.readLine()) != null) {
+				lineNumber++;
 
-		String line;
-		while ((line = csv.readLine()) != null) {
-			lineNumber++;
+				// If it's a comment, skip this line.
+				if (!line.trim().startsWith("#")) {
+					// We use tab separation
+					String[] data = line.split("\t");
+					String wordTypeMarker = data[0];
 
-			// If it's a comment, skip this line.
-			if (!line.trim().startsWith("#")) {
-				// We use tab separation
-				String[] data = line.split("\t");
-				String wordTypeMarker = data[0];
+					// Example line:
+					// POS ID PosS NegS SynsetTerm#sensenumber Desc
+					// a 00009618 0.5 0.25 spartan#4 austere#3 ascetical#2
+					// ascetic#2 practicing great self-denial;...etc
 
-				// Example line:
-				// POS ID PosS NegS SynsetTerm#sensenumber Desc
-				// a 00009618 0.5 0.25 spartan#4 austere#3 ascetical#2
-				// ascetic#2 practicing great self-denial;...etc
-
-				// Is it a valid line? Otherwise, through exception.
-				if (data.length != 6) {
-					throw new IllegalArgumentException(
-							"Incorrect tabulation format in file, line: "
-									+ lineNumber);
-				}
-
-				// Calculate synset score as score = PosS - NegS
-				Double synsetScore = Double.parseDouble(data[2])
-						- Double.parseDouble(data[3]);
-
-				// Get all Synset terms
-				String[] synTermsSplit = data[4].split(" ");
-
-				// Go through all terms of current synset.
-				for (String synTermSplit : synTermsSplit) {
-					// Get synterm and synterm rank
-					String[] synTermAndRank = synTermSplit.split("#");
-					String synTerm = synTermAndRank[0] + "#"
-							+ wordTypeMarker;
-
-					int synTermRank = Integer.parseInt(synTermAndRank[1]);
-					// What we get here is a map of the type:
-					// term -> {score of synset#1, score of synset#2...}
-
-					// Add map to term if it doesn't have one
-					if (!tempDictionary.containsKey(synTerm)) {
-						tempDictionary.put(synTerm,
-								new HashMap<Integer, Double>());
+					// Is it a valid line? Otherwise, through exception.
+					if (data.length != 6) {
+						throw new IllegalArgumentException("Incorrect tabulation format in file, line: " + lineNumber);
 					}
 
-					// Add synset link to synterm
-					tempDictionary.get(synTerm).put(synTermRank,
-							synsetScore);
+					// Calculate synset score as score = PosS - NegS
+					Double synsetScore = Double.parseDouble(data[2]) - Double.parseDouble(data[3]);
+
+					// Get all Synset terms
+					String[] synTermsSplit = data[4].split(" ");
+
+					// Go through all terms of current synset.
+					for (String synTermSplit : synTermsSplit) {
+						// Get synterm and synterm rank
+						String[] synTermAndRank = synTermSplit.split("#");
+						String synTerm = synTermAndRank[0] + "#" + wordTypeMarker;
+
+						int synTermRank = Integer.parseInt(synTermAndRank[1]);
+						// What we get here is a map of the type:
+						// term -> {score of synset#1, score of synset#2...}
+
+						// Add map to term if it doesn't have one
+						if (!tempDictionary.containsKey(synTerm)) {
+							tempDictionary.put(synTerm, new HashMap<Integer, Double>());
+						}
+
+						// Add synset link to synterm
+						tempDictionary.get(synTerm).put(synTermRank, synsetScore);
+					}
 				}
 			}
-		}
 
-		// Go through all the terms.
-		for (Map.Entry<String, HashMap<Integer, Double>> entry : tempDictionary
-				.entrySet()) {
-			String word = entry.getKey();
-			Map<Integer, Double> synSetScoreMap = entry.getValue();
+			// Go through all the terms.
+			for (Map.Entry<String, HashMap<Integer, Double>> entry : tempDictionary.entrySet()) {
+				String word = entry.getKey();
+				Map<Integer, Double> synSetScoreMap = entry.getValue();
 
-			// Calculate weighted average. Weigh the synsets according to
-			// their rank.
-			// Score= 1/2*first + 1/3*second + 1/4*third ..... etc.
-			// Sum = 1/1 + 1/2 + 1/3 ...
-			double score = 0.0;
-			double sum = 0.0;
-			for (Map.Entry<Integer, Double> setScore : synSetScoreMap
-					.entrySet()) {
-				score += setScore.getValue() / (double) setScore.getKey();
-				sum += 1.0 / (double) setScore.getKey();
+				// Calculate weighted average. Weigh the synsets according to
+				// their rank.
+				// Score= 1/2*first + 1/3*second + 1/4*third ..... etc.
+				// Sum = 1/1 + 1/2 + 1/3 ...
+				double score = 0.0;
+				double sum = 0.0;
+				for (Map.Entry<Integer, Double> setScore : synSetScoreMap.entrySet()) {
+					score += setScore.getValue() / (double) setScore.getKey();
+					sum += 1.0 / (double) setScore.getKey();
+				}
+				score /= sum;
+
+				dictionary.put(word, score);
 			}
-			score /= sum;
-
-			dictionary.put(word, score);
-		}
-	} catch (Exception e) {
-		e.printStackTrace();
-	} finally {
-		if (csv != null) {
-			csv.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (csv != null) {
+				csv.close();
+			}
 		}
 	}
-}
 
-public double extract(String word, String pos) {
-	return dictionary.get(word + "#" + pos);
-}
-
-public double getSentiScore(Sentence sentence){
-	double sentimentScore = 0;
-	for(int i=0; i < sentence.length(); i++){
-		String word = sentence.lemma(i);
-		String pos = sentence.posTag(i);
-		if(pos.startsWith("JJ"))
-			pos = "a";
-		else if(pos.startsWith("NN"))
-			pos = "n";
-		else if(pos.startsWith("RB"))
-			pos = "r";
-		else if(pos.startsWith("VB"))
-			pos = "v";
-		if(dictionary.containsKey(word + "#" + pos)){
-			sentimentScore += this.extract(word, pos);
-			//System.out.println(word + " # " + pos + " * "+ this.extract(word, pos));
-		}
+	public double extract(String word, String pos) {
+		return dictionary.get(word + "#" + pos);
 	}
-	return sentimentScore;
-}
+
+	
+	public double getSentiScore(Sentence sentence) {
+		double sentimentScore = 0;
+		for (int i = 0; i < sentence.length(); i++) {
+			String word = sentence.lemma(i);
+			String pos = sentence.posTag(i);
+			if (pos.startsWith("JJ"))
+				pos = "a";
+			else if (pos.startsWith("NN"))
+				pos = "n";
+			else if (pos.startsWith("RB"))
+				pos = "r";
+			else if (pos.startsWith("VB"))
+				pos = "v";
+			if (dictionary.containsKey(word + "#" + pos)) {
+				sentimentScore += this.extract(word, pos);
+			}
+		}
+		return sentimentScore;
+	}
 
 }
--- src/kr/ac/kookmin/cs/bigdata/SentimentScore.java
+++ src/kr/ac/kookmin/cs/bigdata/SentimentScore.java
@@ -24,95 +24,91 @@ import com.google.protobuf.TextFormat.ParseException;
 import edu.stanford.nlp.simple.Sentence;
 
 public class SentimentScore extends Configured implements Tool {
-	private static SentiWordNet sentiWordNet ;
-    public static void main(String[] args) throws Exception {
-    	System.out.println(Arrays.toString(args));
-        int res = ToolRunner.run(new Configuration(), new SentimentScore(), args);
-        System.exit(res);
-    }
+	private static SentiWordNet sentiWordNet;
 
-    @Override
-    public int run(String[] args) throws Exception {
-    	sentiWordNet = new SentiWordNet("SentiWordNet_3.0.0_20130122.txt");
-    	 Path p1 = new Path(args[0]);
-         Path p2 = new Path(args[1]);
-         Path out = new Path(args[2]);
-         
-         Job job = Job.getInstance(getConf());
-         job.setJarByClass(SentimentScore.class);
-         job.setOutputKeyClass(Text.class);
-         job.setOutputValueClass(Text.class);
+	public static void main(String[] args) throws Exception {
+		System.out.println(Arrays.toString(args));
+		int res = ToolRunner.run(new Configuration(), new SentimentScore(), args);
+		System.exit(res);
+	}
 
-         job.setMapperClass(Map.class);
-         job.setReducerClass(Reduce.class);
+	@Override
+	public int run(String[] args) throws Exception {
+		sentiWordNet = new SentiWordNet("SentiWordNet_3.0.0_20130122.txt");
+		Path p1 = new Path(args[0]);
+		Path p2 = new Path(args[1]);
+		Path out = new Path(args[2]);
 
-        job.setOutputFormatClass(TextOutputFormat.class);
-         
-        MultipleInputs.addInputPath(job, p1, TextInputFormat.class, Map.class);
-        MultipleInputs.addInputPath(job, p2, TextInputFormat.class, Map.class);
-        
-        FileOutputFormat.setOutputPath(job, out);
-        job.waitForCompletion(true);
- 		return 0;
+		Job job = Job.getInstance(getConf());
+		job.setJarByClass(SentimentScore.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
 
+		job.setMapperClass(Map.class);
+		job.setReducerClass(Reduce.class);
 
+		job.setOutputFormatClass(TextOutputFormat.class);
 
-    }
-   
-    public static class Map extends Mapper<LongWritable, Text, Text, Text> {
-        private Text asin = new Text();
-        private Text text = new Text();
+		MultipleInputs.addInputPath(job, p1, TextInputFormat.class, Map.class);
+		MultipleInputs.addInputPath(job, p2, TextInputFormat.class, Map.class);
 
-        @Override
-        public void map(LongWritable key, Text value, Context context)
-                throws IOException, InterruptedException {
-        	try{
-            JsonObject jsonObject = new JsonParser().parse(value.toString()).getAsJsonObject();
-            asin.set(jsonObject.get("asin").toString());
-            if(jsonObject.has("reviewText"))
-            	text.set(jsonObject.get("reviewText").toString());
-            else if(jsonObject.has("salesRank"))
-            	text.set(jsonObject.getAsJsonObject("salesRank").get("Books").toString());
-            else
-            	text.set("-1");
-            
-            context.write(asin, text);
-        	}
-        	catch(ParseException e){
-        		e.printStackTrace();
-        	}
-        }
-    }
+		FileOutputFormat.setOutputPath(job, out);
+		job.waitForCompletion(true);
+		return 0;
 
-    public static class Reduce extends Reducer<Text, Text, Text, Text> {
-    	
-    	Text mergeText = new Text();
-    	String merge = "";
-    	 public void reduce(Text key, Iterable<Text> values, Context context)
-                 throws IOException, InterruptedException {
-    		 	int count = 0;
-    		 	double score = 0;
-    		 	long rankScore = -1;
-    		 	for (Text value : values) {
-    		 	    char token = 'o';
-    		 	    count++;
-    		 	    String str = value.toString();
-    		 	  
-    		 	   if(!str.isEmpty())
-    		 		  token = str.charAt(0);
-    		 	  
- 	        	   if(Character.isDigit(token))
- 	        		  rankScore = Long.valueOf(str);
- 	        	   else
- 	        	   {
- 	        		  count++;
- 	        		  score += sentiWordNet.getSentiScore(new Sentence(str));
- 	        	   }
- 	           }
-    		 	double avg = score / count;
-    		 	merge = String.valueOf(avg) + "	::	" + String.valueOf(rankScore);
-    		 	mergeText.set(merge);
- 	           context.write(key, mergeText);
-        }
-    }
+	}
+
+	public static class Map extends Mapper<LongWritable, Text, Text, Text> {
+		private Text asin = new Text();
+		private Text text = new Text();
+
+		@Override
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			try {
+				JsonObject jsonObject = new JsonParser().parse(value.toString()).getAsJsonObject();
+				asin.set(jsonObject.get("asin").toString());
+				if (jsonObject.has("reviewText"))
+					text.set(jsonObject.get("reviewText").toString());
+				else if (jsonObject.has("salesRank"))
+					text.set(jsonObject.getAsJsonObject("salesRank").get("Books").toString());
+				else
+					text.set("-1");
+
+				context.write(asin, text);
+			} catch (ParseException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static class Reduce extends Reducer<Text, Text, Text, Text> {
+
+		Text mergeText = new Text();
+		String merge = "";
+
+		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+			int count = 0;
+			double score = 0;
+			long rankScore = -1;
+			for (Text value : values) {
+				char token = 'o';
+				count++;
+				String str = value.toString();
+
+				if (!str.isEmpty())
+					token = str.charAt(0);
+
+				if (Character.isDigit(token))
+					rankScore = Long.valueOf(str);
+				else {
+					count++;
+					score += sentiWordNet.getSentiScore(new Sentence(str));
+				}
+			}
+			double avg = score / count;
+			merge = String.valueOf(avg) + "	::	" + String.valueOf(rankScore);
+			mergeText.set(merge);
+			context.write(key, mergeText);
+		}
+	}
 }
